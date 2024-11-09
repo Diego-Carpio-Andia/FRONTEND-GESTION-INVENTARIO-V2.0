1ae30189acb7b6e5d8148a11fcb7fd36bea20e4e
Diego Carpio Andia <101060746+Diego-Carpio-Andia@users.noreply.github.com>
2024-11-09 10:31:33 -0500

    Update global.jsx

--- src/helpers/global.jsx
+++ src/helpers/global.jsx
@@ -1,6 +1,6 @@
 export const Global = {
     //url: "https://api-backend-tesis.azurewebsites.net/api/",
     //url2: "https://modelo-tesis.azurewebsites.net/"
-    url: "despliegue-backend-inventario.azurewebsites.net/api/",
-    url2: "https://modelo-tesis.azurewebsites.net/" 
+    url: "https://despliegue-backend-inventario.azurewebsites.net/api/",
+    url2: "https://depliegue-backend-prediccion.azurewebsites.net" 
 }
